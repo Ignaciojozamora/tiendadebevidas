158c067d39c758a51922f191003223d65fe49699
Ignacio Zamora <ignaciojo.zamora@gmail.com>
2022-09-28 21:02:32 -0300

    navbar con widget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import NavScroll from './components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
     <NavScroll/>
+    
     </div>
   );
 }
---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const CartWidget = ()=>{
+    return (
+        <div>
+        <FontAwesomeIcon icon={faCoffee}/>
+        </div>
+    )
+
+}
+
+export default CartWidget()
