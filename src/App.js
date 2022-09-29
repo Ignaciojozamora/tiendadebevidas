336a4b816e86e0caedf578970aadaebbf6b09000
Ignacio Zamora <ignaciojo.zamora@gmail.com>
2022-09-28 21:58:09 -0300

    se agrega otra libreria para cartwidget

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
 import NavScroll from './components/NavBar/NavBar';
-
+import CartWidget from './components/CartWidget/CartWidget';
 
 function App() {
   return (
     <div className="App">
     <NavScroll/>
-    
+    <CartWidget/>
     </div>
   );
 }
--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,31 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/* import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const CartWidget = ()=>{
     return (
         <div>
         <FontAwesomeIcon icon={faCoffee}/>
+        </div>
+    )
+
+}
+
+export default CartWidget()
+ */
+
+import * as React from 'react';
+import IconButton from '@mui/material/Button';
+import StyledBadge from '@mui/material/Badge';
+import ShoppingCartIcon from '@mui/material/ShoppingCartIcon';
+
+const CartWidget = ()=>{
+    return (
+        <div>
+        <IconButton aria-label="cart">
+            <StyledBadge badgeContent={4} color="secondary">
+            <ShoppingCartIcon />
+            </StyledBadge>
+        </IconButton>
         </div>
     )
 
